47fef39cb8076552d11e6230f16048bef83bfe29
xyyss110 <2249884984@qq.com>
2018-06-11 10:31:38 +0800

    水流测速主体框架搭建v1.0 fftw3库需要添加，忽略

---
+++ openpiv-c--qt/tools.cpp
@@ -0,0 +1,56 @@
+﻿#include "tools.h"
+
+
+
+tools::tools()
+{
+}
+
+
+tools::~tools()
+{
+}
+
+int tools::extract_video(string file_all_name, string file_path, string base_name)
+{
+	//打开视频文件：其实就是建立一个VideoCapture结构  
+	VideoCapture capture;
+	//检测是否正常打开:成功打开时，isOpened返回ture  
+	if (!capture.open(file_all_name)) {
+		cout << "video name error!" << endl;
+		return -1;
+	}
+	//获取整个帧数  
+	long totalFrameNumber = capture.get(CV_CAP_PROP_FRAME_COUNT);
+	//设置开始帧()  
+	long frameToStart = 0;
+	capture.set(CV_CAP_PROP_POS_FRAMES, frameToStart);
+	//获取帧率  
+	double rate = capture.get(CV_CAP_PROP_FPS);
+	//承载每一帧的图像  
+	Mat frame;
+	QProgressDialog process;
+	process.setLabelText("processing...");
+	process.setRange(0, totalFrameNumber);
+	process.setModal(true);
+	process.setCancelButtonText("cancel");
+
+	for (int i = 0; i < totalFrameNumber; ++i) {
+		if (!capture.read(frame))
+			break;
+		Mat image;
+		transpose(frame, image);
+		ostringstream temp_string;
+		temp_string.fill('0');
+		temp_string.width(5);
+		temp_string << i;
+		string temp = file_path + "/" + base_name + "_" + temp_string.str() + ".jpg";
+		imwrite(temp, image);
+		process.setValue(i);
+		if (process.wasCanceled())
+			break;
+	}
+	//关闭视频文件
+	capture.release();
+	return 0;
+}
---
+++ openpiv-c--qt/tools.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include <QDialog>
+#include <QProgressDialog>
+
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"  
+#include "opencv2/core/core.hpp"
+#include "opencv2/features2d/features2d.hpp"  
+using namespace cv;
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+#include <fstream>
+using namespace std;
+class tools
+{
+public:
+	tools();
+	~tools();
+	int extract_video(string file_all_name, string file_path, string base_name);
+};
+
