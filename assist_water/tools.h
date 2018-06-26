4e53b23a06e5fad523f1c1b3fbd0af6fffaf7778
xy <2249884984@qq.com>
2018-06-26 16:47:12 +0800

    no message

---
+++ assist_water/tools.h
@@ -0,0 +1,5 @@
+﻿#pragma once
+
+void exp(const float *_x, float *y, int n);
+void fastAtan2(const float *Y, const float *X, float *angle, int len, bool angleInDegrees = true);
+void magnitude(const float* x, const float* y, float* mag, int len);
